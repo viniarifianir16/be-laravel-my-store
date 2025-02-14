2a0bc0aa128013f121a771a641278a402237f139
viniarifianir16 <viniarifianir16@gmail.com>
2025-02-14 20:52:35 +0700

    fix: fix auth config

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,8 +14,7 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->api(prepend: [
             \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
-            \Illuminate\Routing\Middleware\ThrottleRequests::class . ':api',
-            \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            \Illuminate\Http\Middleware\HandleCors::class,
         ]);
 
         $middleware->alias([
