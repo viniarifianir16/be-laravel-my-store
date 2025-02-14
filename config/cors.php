355dbb61f166d787660772ec8709ae0a88de5b80
viniarifianir16 <viniarifianir16@gmail.com>
2025-02-14 18:50:52 +0700

    fix: fix cors

--- config/cors.php
+++ config/cors.php
@@ -19,10 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => [
-        'https://fe-react-my-store.vercel.app',
-        'http://localhost:5173'
-    ],
+    'allowed_origins' => ['https://fe-react-my-store.vercel.app', 'http://localhost:5173'],
 
     'allowed_origins_patterns' => [],
 
