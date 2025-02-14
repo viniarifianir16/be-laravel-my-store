5f63c922e26090838a098d9f8a6f3353a0fb509d
viniarifianir16 <viniarifianir16@gmail.com>
2025-02-14 09:15:08 +0700

    feat: update route

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Laravel\Sanctum\Http\Controllers\CsrfCookieController;
 
 Route::get('/', function () {
     return ['Laravel' => app()->version()];
 });
 
-require __DIR__.'/auth.php';
+Route::get('/sanctum/csrf-cookie', [CsrfCookieController::class, 'show']);
+
+require __DIR__ . '/auth.php';
